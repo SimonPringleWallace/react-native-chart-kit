cedc985a0905148e3f7d88a35f5f6f013fc4a43c
SimonPringleWallace <s.pringlewallace@gmail.com>
2020-01-09 00:37:22 -0500

    [ADD Add Caches for Chart utils - Memoize each function in Chart Utils to improve run times

--- src/chart-utils.js
+++ src/chart-utils.js
@@ -1,31 +1,66 @@
+const calcHeightCache = {};
+const calcScalerCache = {};
+const calcBaseHeightCache = {};
+
 export function calcScaler(minValue, maxValue, fromZero) {
+  const argString = `${minValue}${maxValue}${fromZero}`;
+  let result;
+  if (calcScalerCache[argString]) {
+    return calcScalerCache[argString];
+  }
   if (fromZero) {
-    return Math.max(maxValue, 0) - Math.min(minValue, 0) || 1;
+    result = Math.max(maxValue, 0) - Math.min(minValue, 0) || 1;
+    calcScalerCache[argString] = result;
+    return result;
   } else {
-    return maxValue - minValue || 1;
+    result = maxValue - minValue || 1;
+    calcScalerCache[argString] = result;
+    return result;
   }
 }
 
 export function calcBaseHeight(minValue, maxValue, height, fromZero) {
+  const argString = `${minValue}${maxValue}${height}${fromZero}`;
+  let result;
+  if (calcBaseHeightCache[argString]) {
+    return calcBaseHeightCache[argString];
+  }
   if (minValue >= 0 && maxValue >= 0) {
-    return height;
+    result = height;
+    calcBaseHeightCache[argString] = result;
+    return result;
   } else if (minValue < 0 && maxValue <= 0) {
-    return 0;
+    result = 0;
+    calcBaseHeightCache[argString] = result;
+    return result;
   } else if (minValue < 0 && maxValue > 0) {
-    return (height * maxValue) / calcScaler(minValue, maxValue, fromZero);
+    result = (height * maxValue) / calcScaler(minValue, maxValue, fromZero);
+    calcBaseHeightCache[argString] = result;
+    return result;
   }
 }
 
 export function calcHeight(val, height, minValue, maxValue, fromZero) {
+  const argString = `${val}${height}${minValue}${maxValue}${fromZero}`;
+  let result;
+  if (calcHeightCache[argString]) {
+    return calcHeightCache[argString];
+  }
   if (minValue < 0 && maxValue > 0) {
-    return height * (val / calcScaler(minValue, maxValue, fromZero));
+    result = height * (val / calcScaler(minValue, maxValue, fromZero));
+    calcHeightCache[argString] = result;
+    return result;
   } else if (minValue >= 0 && maxValue >= 0) {
-    return fromZero
+    result = fromZero
       ? height * (val / calcScaler(minValue, maxValue, fromZero))
       : height * ((val - minValue) / calcScaler(minValue, maxValue, fromZero));
+    calcHeightCache[argString] = result;
+    return result;
   } else if (minValue < 0 && maxValue <= 0) {
-    return fromZero
+    result = fromZero
       ? height * (val / calcScaler(minValue, maxValue, fromZero))
       : height * ((val - maxValue) / calcScaler(minValue, maxValue, fromZero));
+    calcHeightCache[argString] = result;
+    return result;
   }
 }
